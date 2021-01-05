ba38dc9c0d7a7249d2c6dad8ef9ad6e21706f1d5
ShayElkana <Shay.Elkana@Gmail.com>
2021-01-05 12:50:16 +0200

    Init

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.post('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.listen(process.env.PORT || 3002);
